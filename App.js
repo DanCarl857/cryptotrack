b892603226ca524916963b689216f8329558e679
Daniel Carlson <dancarl758@gmail.com>
2019-08-19 21:05:07 +0100

    Completed redux configuration

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 import React, {Component} from 'react';
 import { View } from 'react-native'
 import { Router, Scene, ActionConst } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/Ionicons'
+
+// Redux configuration
+import { Provider } from 'react-redux'
+import store from './src/store'
 
 // Styling
 import theme from './src/styles/theme.style'
@@ -21,7 +18,7 @@ import Summary from './src/screens/Home/Summary/Summary.component'
 import Cryptocurrency from './src/screens/Home/Cryptocurrency/Cryptocurrency.component'
 
 const TabIcon = ({ focused, iconName }) => {
-  var color = focused ? theme.PRIMARY_COLOR : 'grey'
+  let color = focused ? theme.SECONDARY_COLOR : 'grey'
   return (
     <View style={{flex:1, flexDirection:'column', alignSelf:'center', ...common.center }}>
       <Icon style={{color: color}} name={iconName || "circle"} size={20}/>
@@ -32,33 +29,35 @@ const TabIcon = ({ focused, iconName }) => {
 export default class App extends Component {
   render() {
     return (
-      <Router
-        navigationBarStyle={{ backgroundColor: theme.WHITE }}
-        tintColor={theme.TEXT_COLOR}
-        titleStyle={{ color: theme.TEXT_COLOR, fontSize: theme.FONT_SIZE_LARGE, fontWeight: theme.FONT_WEIGHT_MEDIUM }}>
-        <Scene key="root">
-          <Scene key="start" component={Start} hideNavBar />
-          <Scene
-            key="home"
-            tabs={true}
-            tabBarStyle={{ backgroundColor: theme.WHITE, paddingTop: 10 }}
-            activeTintColor={theme.PRIMARY_COLOR}
-            hideNavBar type={ActionConst.RESET}
-            lazy
-            showLabel={true}
-            labelStyle={{fontWeight: theme.FONT_WEIGHT_MEDIUM}}>
-            <Scene key="summary" icon={TabIcon} title="Summary" iconName="ios-calendar">
-              <Scene key="summary" component={Summary} title="Summary" />
-            </Scene>
-            <Scene key="cryptocurrency" icon={TabIcon} title="Cryptocurrency" iconName="ios-paper-plane">
-              <Scene key="cryptocurrency" component={Cryptocurrency} title="Cryptocurrency Listings" />
-            </Scene>
-            <Scene key="settings" icon={TabIcon} title="Settings" iconName="ios-cog">
-              <Scene key="settings" component={Settings} hideNavBar />
+      <Provider store={store}>
+        <Router
+          navigationBarStyle={{ backgroundColor: theme.WHITE }}
+          tintColor={theme.TEXT_COLOR}
+          titleStyle={{ color: theme.TEXT_COLOR, fontSize: theme.FONT_SIZE_LARGE, fontWeight: theme.FONT_WEIGHT_MEDIUM }}>
+          <Scene key="root">
+            <Scene key="start" component={Start} hideNavBar />
+            <Scene
+              key="home"
+              tabs={true}
+              tabBarStyle={{ backgroundColor: theme.WHITE, paddingTop: 10 }}
+              activeTintColor={theme.SECONDARY_COLOR}
+              hideNavBar type={ActionConst.RESET}
+              lazy
+              showLabel={true}
+              labelStyle={{fontWeight: theme.FONT_WEIGHT_MEDIUM}}>
+              <Scene key="summary" icon={TabIcon} title="Summary" iconName="ios-calendar">
+                <Scene key="summary" component={Summary} title="Summary" />
+              </Scene>
+              <Scene key="cryptocurrency" icon={TabIcon} title="Cryptocurrency" iconName="ios-paper-plane">
+                <Scene key="cryptocurrency" component={Cryptocurrency} title="Cryptocurrency Listings" />
+              </Scene>
+              <Scene key="settings" icon={TabIcon} title="Settings" iconName="ios-cog">
+                <Scene key="settings" component={Settings} hideNavBar />
+              </Scene>
             </Scene>
           </Scene>
-        </Scene>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
