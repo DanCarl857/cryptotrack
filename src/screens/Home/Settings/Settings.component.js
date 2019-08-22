9da0c1e502819993d9f951a5d141c9b4d3223673
Daniel Carlson <dancarl758@gmail.com>
2019-08-22 09:26:14 +0100

    Finished basic application:

--- src/screens/Home/Settings/Settings.component.js
+++ src/screens/Home/Settings/Settings.component.js
@@ -1,65 +1,86 @@
-import React, { Component } from 'react'
-import { Text, View, ScrollView, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Text, View, ScrollView, Image, TouchableOpacity, Linking } from 'react-native'
 import styles from './Settings.component.style'
 
-const Settings = () => (
-    <View style={styles.container}>
-        <View style={styles.topContainer}>
-            <Text style={styles.name}>CoinMarketCap</Text>
-            <Text style={styles.ID}>https://coinmarketcap.com/api/documentation/</Text>
+const Settings = () => {
+
+    openLink = url => Linking.openURL(url).catch((err) => console.error('An error occurred', err))
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text style={styles.name}>CoinMarketCap</Text>
+                <TouchableOpacity onPress={() => openLink('https://coinmarketcap.com/api/documentation/')}>
+                    <Text style={styles.ID}>https://coinmarketcap.com/api/documentation/</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.bottomContainer}>
+                <ScrollView>
+                    <View style={styles.headingContainer}>
+                        <Text style={styles.heading}>About</Text>
+                    </View>
+                    <TouchableOpacity style={styles.row} onPress={() => openLink('https://coinmarketcap.com/')}>
+                        <View style={styles.col1}>
+                            <Image
+                                source={require('./../../../assets/settings/web.png')}
+                                style={styles.imgStyle}
+                            />
+                        </View>
+                        <View style={styles.col4}>
+                            <Text>Visit our website for more information</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.row}>
+                        <View style={styles.col1}>
+                            <Image
+                                source={require('./../../../assets/settings/rate.png')}
+                                style={styles.imgStyle}
+                            />
+                        </View>
+                        <View style={styles.col4}>
+                            <Text>Rate the app</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.row} onPress={() => openLink('https://coinmarketcap.com/terms/')}>
+                        <View style={styles.col1}>
+                            <Image
+                                source={require('./../../../assets/settings/terms.png')}
+                                style={styles.imgStyle}
+                            />
+                        </View>
+                        <View style={styles.col4}>
+                            <Text>Terms and Conditions</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.row} onPress={() => openLink('https://coinmarketcap.com/privacy/')}>
+                        <View style={styles.col1}>
+                            <Image
+                                source={require('./../../../assets/settings/privacy.png')}
+                                style={styles.imgStyle}
+                            />
+                        </View>
+                        <View style={styles.col4}>
+                            <Text>Privacy Policy</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={styles.headingContainer}>
+                        <Text style={styles.heading}>Developer</Text>
+                    </View>
+                    <TouchableOpacity style={styles.row} onPress={() => openLink('https://github.com/DanCarl857')}>
+                        <View style={styles.col1}>
+                            <Image
+                                source={require('./../../../assets/settings/user.png')}
+                                style={styles.imgStyle}
+                            />
+                        </View>
+                        <View style={styles.col4}>
+                            <Text>About Developer</Text>
+                        </View>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
         </View>
-        <View style={styles.bottomContainer}>
-            <ScrollView>
-                <View style={styles.headingContainer}>
-                    <Text style={styles.heading}>About</Text>
-                </View>
-                <TouchableOpacity style={styles.row}>
-                    <View style={styles.col1}>
-                        <Image
-                            source={require('./../../../assets/settings/web.png')}
-                            style={styles.imgStyle}
-                        />
-                    </View>
-                    <View style={styles.col4}>
-                        <Text>Visit our website</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.row}>
-                    <View style={styles.col1}>
-                        <Image
-                            source={require('./../../../assets/settings/rate.png')}
-                            style={styles.imgStyle}
-                        />
-                    </View>
-                    <View style={styles.col4}>
-                        <Text>Rate the app</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.row}>
-                    <View style={styles.col1}>
-                        <Image
-                            source={require('./../../../assets/settings/terms.png')}
-                            style={styles.imgStyle}
-                        />
-                    </View>
-                    <View style={styles.col4}>
-                        <Text>Terms and Conditions</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.row}>
-                    <View style={styles.col1}>
-                        <Image
-                            source={require('./../../../assets/settings/privacy.png')}
-                            style={styles.imgStyle}
-                        />
-                    </View>
-                    <View style={styles.col4}>
-                        <Text>Privacy Policy</Text>
-                    </View>
-                </TouchableOpacity>
-            </ScrollView>
-        </View>
-    </View>
-)
+    )
+}
 
 export default Settings
